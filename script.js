142b72d75d1901c850f341b40fbf61916e92317f
Sunil-Hegde <sunil1hegde@gmail.com>
2023-09-23 11:59:27 +0530

    Added side panel js and script

---
+++ script.js
@@ -0,0 +1,12 @@
+const hamburgerIcon = document.querySelector('.hamburger-icon');
+const sidePanel = document.querySelector('.side-panel');
+
+hamburgerIcon.addEventListener('click', () => {
+    // Toggle the 'open' class to show/hide the side panel
+    sidePanel.classList.toggle('open');
+});
+
+// Function to close the side panel (if needed)
+function closeSidePanel() {
+    sidePanel.classList.remove('open');
+}
