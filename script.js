497e2144763e79f26416bb2e2536381a4b3c8224
Arya-Pai <aryabhaskarpai@gmail.com>
2023-09-23 15:24:23 +0530

    Hamburger icon to right footer made ready

---
+++ script.js
@@ -0,0 +1,9 @@
+const hamburgerIcon = document.querySelector('.hamburger-icon');
+const sidePanel = document.querySelector('.side-panel');
+
+hamburgerIcon.addEventListener('click', () => {
+    // Toggle the 'open' class to show/hide the side panel
+    sidePanel.classList.toggle('open');
+});
+
+// Function to close the side panel (if needed)
